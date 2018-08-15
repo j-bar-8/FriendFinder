aabc2b6febe0fa0115d5c3776460ad6bc95603a9
j-bar-8 <josh.barkat@gmail.com>
2018-08-15 00:41:36 -0400

    fixed api route

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,18 +1,44 @@
 // DATA
 // =====================================================
-var friendData = require("../data/friendData");
+var friendArray = require("../data/friendData");
 
 
 // ROUTING
 // =====================================================
 module.exports = (app) => {
     app.get("/api/friends", (req, res) => {
-      res.json(friendData);
+      res.json(friendArray);
     });
   
     app.post("/api/friends", (req, res) => {
-        friendData.push(req.body);
-        res.json(true);
+        var bestMatch = {
+          name: "",
+          photo: "",
+          friendDifference: 0
+        };
+
+        var userData = req.body;
+        var userName = userData.name;
+        var userPhoto = userData.photo;
+        var userScores = userData.scores;
+        
+        var totalDifference = 0;
+
+        for (var i=0; i<friendArray.length; i++) {
+          totalDifference = 0
+          for (var j=o; i<friendArray[i].scores[j]; j++) {
+            totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+            if (totalDifference <= bestMatch.friendDifference) {
+              bestMatch.name = friends[i].name;
+              bestMatch.photo = friends[i].photo;
+              bestMatch.friendDifference = totalDifference;
+            }
+          }
+        }
+
+        friendArray.push(userData);
+
+        res.json(bestMatch);
     });
   
   };
